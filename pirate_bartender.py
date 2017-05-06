5787d5b7e1b7a77cb0915c85efaa9eced48afb2c
jsudufour <jsudufour@gmail.com>
2017-05-06 11:23:12 -0400

    Added drink_preferences function - asks the user what flavours they prefer.

--- pirate_bartender.py
+++ pirate_bartender.py
@@ -1,9 +1,9 @@
 questions = {
-    "strong": "Do ye like yer drinks strong?",
-    "salty": "Do ye like it with a salty tang?",
-    "bitter": "Are ye a lubber who likes it bitter?",
-    "sweet": "Would ye like a bit of sweetness with yer poison?",
-    "fruity": "Are ye one for a fruity finish?",
+    "strong": "Do ye like yer drinks strong? ",
+    "salty": "Do ye like it with a salty tang? ",
+    "bitter": "Are ye a lubber who likes it bitter? ",
+    "sweet": "Would ye like a bit of sweetness with yer poison? ",
+    "fruity": "Are ye one for a fruity finish? ",
 }
 
 ingredients = {
@@ -14,13 +14,41 @@ ingredients = {
     "fruity": ["slice of orange", "dash of cassis", "cherry on top"],
 }
 
+preferences = {
+    # "strong": False,
+    # "salty": False,
+    # "bitter": False,
+    # "sweet": False,
+    # "fruity": False,
+}
+
 def drink_preferences():
     '''Asks each question in the questions dictionary and saves all the positive responses in a new dictionary'''
-    preferences = {}
 
     print("Let's see what yer perfect drink is like! Answer yes/y or no/n to each of these questions: ")
-    for q, a in preferences.items():
+    for c, q in questions.items():
+        response_valid = False
         response = input(q)
-        if (response == "yes" or response == "y"):
+        while not response_valid:
+            if response == "y" or response == "yes" or response == "n" or response == "no":
+                response_valid = True
+                add_to_dictionary(c, response)
+            else:
+                print("Ye need to answer yes or no!")
+                response = input(q)
 
-    preferences.update({'item3': 3})
+
+def add_to_dictionary(flavour, answer):
+    '''Takes a flavour and user input and adds its equivalent boolean to a dictionary'''
+
+    if answer == "y" or answer == "yes":
+        preferences[flavour] = True
+    else:
+        preferences[flavour] = False
+
+
+
+drink_preferences()
+
+for flavour, like in preferences.items():
+    print(flavour, like)
